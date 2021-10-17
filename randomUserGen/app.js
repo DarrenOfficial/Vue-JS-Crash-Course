a770ad1b2a26b35415e5852dcd76421badb0fde0
DarrenOfficial <github@darrennathanael.com>
2021-10-17 05:52:46 -0400

    me gonna sleep

--- randomUserGen/app.js
+++ randomUserGen/app.js
@@ -1,5 +1,26 @@
 const app = Vue.createApp({
-    template: '<h1>pepepopo</h1>'
+    // template: '<h1>Bonjour {{firstName}}</h1>',
+    data() {
+        return {
+            firstName: 'Darren',
+            lastName: 'Nathan',
+            email: 'github@darrennathanael.com',
+            gender: 'male',
+            sexuality: 'straight',
+            picture: 'https://thispersondoesnotexist.com/image',
+        }
+    },
+    methods: {
+        async getUser() {
+            const res = await fetch()
+            this.firstName = 'John',
+            this.lastName = 'Doe',
+            this.email = 'john@darrennathanael.com',
+            this.gender = 'female',
+            this.sexuality = 'straight',
+            this.picture = 'https://thispersondoesnotexist.com/image'
+        },
+    },
 })
 
 app.mount('#app')
